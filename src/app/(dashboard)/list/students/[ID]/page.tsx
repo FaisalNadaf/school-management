ada66cbaf6274621ce49d3367efba062ef49c813
FaisalNadaf <faisalfnadaf@gmail.com>
2024-11-19 22:21:27 +0530

    all lists

--- src/app/(dashboard)/list/students/[ID]/page.tsx
+++ src/app/(dashboard)/list/students/[ID]/page.tsx
@@ -1,16 +1,38 @@
 /** @format */
-
-import BigCalendar from "@/components/BigCalender";
+import Announcement from "@/components/Announcement";
+import FormContainer from "@/components/FormContainer";
 import Performance from "@/components/Performance";
-
-import { role } from "@/lib/data";
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { Class, Student } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
-import AnnouncementListPage from "../../announcements/page";
-import Announcement from "@/components/Announcement";
-import FormModal from "@/components/FormModal";
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
 
-const SingleStudentPage = () => {
+const SingleStudentPage = async ({
+	params: { id },
+}: {
+	params: { id: string };
+}) => {
+	const { sessionClaims } = auth();
+	const role = (sessionClaims?.metadata as { role?: string })?.role;
+
+	const student:
+		| (Student & {
+				class: Class & { _count: { lessons: number } };
+		  })
+		| null = await prisma.student.findUnique({
+		where: { id },
+		include: {
+			class: { include: { _count: { select: { lessons: true } } } },
+		},
+	});
+
+	if (!student) {
+		return notFound();
+	}
+
 	return (
 		<div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
 			{/* LEFT */}
@@ -18,10 +40,10 @@ const SingleStudentPage = () => {
 				{/* TOP */}
 				<div className="flex flex-col lg:flex-row gap-4">
 					{/* USER INFO CARD */}
-					<div className="bg-faisal_dev_SeaGreen py-6 px-4 rounded-md flex-1 flex gap-4">
+					<div className="bg-lamaSky py-6 px-4 rounded-md flex-1 flex gap-4">
 						<div className="w-1/3">
 							<Image
-								src="https://images.pexels.com/photos/5414817/pexels-photo-5414817.jpeg?auto=compress&cs=tinysrgb&w=1200"
+								src={student.img || "/noAvatar.png"}
 								alt=""
 								width={144}
 								height={144}
@@ -30,25 +52,14 @@ const SingleStudentPage = () => {
 						</div>
 						<div className="w-2/3 flex flex-col justify-between gap-4">
 							<div className="flex items-center gap-4">
-								<h1 className="text-xl font-semibold">Student Snyder</h1>
+								<h1 className="text-xl font-semibold">
+									{student.name + " " + student.surname}
+								</h1>
 								{role === "admin" && (
-									<FormModal
+									<FormContainer
 										table="student"
 										type="update"
-										data={{
-											id: 1,
-											username: "deanguerrero",
-											email: "deanguerrero@gmail.com",
-											password: "password",
-											firstName: "Dean",
-											lastName: "Guerrero",
-											phone: "+1 234 567 89",
-											address: "1234 Main St, Anytown, USA",
-											bloodType: "A+",
-											dateOfBirth: "2000-01-01",
-											sex: "male",
-											img: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200",
-										}}
+										data={student}
 									/>
 								)}
 							</div>
@@ -63,7 +74,7 @@ const SingleStudentPage = () => {
 										width={14}
 										height={14}
 									/>
-									<span>A+</span>
+									<span>{student.bloodType}</span>
 								</div>
 								<div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
 									<Image
@@ -72,7 +83,9 @@ const SingleStudentPage = () => {
 										width={14}
 										height={14}
 									/>
-									<span>January 2025</span>
+									<span>
+										{new Intl.DateTimeFormat("en-GB").format(student.birthday)}
+									</span>
 								</div>
 								<div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
 									<Image
@@ -81,7 +94,7 @@ const SingleStudentPage = () => {
 										width={14}
 										height={14}
 									/>
-									<span>user@gmail.com</span>
+									<span>{student.email || "-"}</span>
 								</div>
 								<div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
 									<Image
@@ -90,7 +103,7 @@ const SingleStudentPage = () => {
 										width={14}
 										height={14}
 									/>
-									<span>+1 234 567</span>
+									<span>{student.phone || "-"}</span>
 								</div>
 							</div>
 						</div>
@@ -106,10 +119,9 @@ const SingleStudentPage = () => {
 								height={24}
 								className="w-6 h-6"
 							/>
-							<div className="">
-								<h1 className="text-xl font-semibold">90%</h1>
-								<span className="text-sm text-gray-400">Attendance</span>
-							</div>
+							<Suspense fallback="loading...">
+								{/* <StudentAttendanceCard id={student.id} /> */}
+							</Suspense>
 						</div>
 						{/* CARD */}
 						<div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
@@ -121,7 +133,9 @@ const SingleStudentPage = () => {
 								className="w-6 h-6"
 							/>
 							<div className="">
-								<h1 className="text-xl font-semibold">6th</h1>
+								<h1 className="text-xl font-semibold">
+									{student.class.name.charAt(0)}th
+								</h1>
 								<span className="text-sm text-gray-400">Grade</span>
 							</div>
 						</div>
@@ -135,7 +149,9 @@ const SingleStudentPage = () => {
 								className="w-6 h-6"
 							/>
 							<div className="">
-								<h1 className="text-xl font-semibold">18</h1>
+								<h1 className="text-xl font-semibold">
+									{student.class._count.lessons}
+								</h1>
 								<span className="text-sm text-gray-400">Lessons</span>
 							</div>
 						</div>
@@ -149,7 +165,7 @@ const SingleStudentPage = () => {
 								className="w-6 h-6"
 							/>
 							<div className="">
-								<h1 className="text-xl font-semibold">6A</h1>
+								<h1 className="text-xl font-semibold">{student.class.name}</h1>
 								<span className="text-sm text-gray-400">Class</span>
 							</div>
 						</div>
@@ -158,7 +174,10 @@ const SingleStudentPage = () => {
 				{/* BOTTOM */}
 				<div className="mt-4 bg-white rounded-md p-4 h-[800px]">
 					<h1>Student&apos;s Schedule</h1>
-					<BigCalendar />
+					{/* <BigCalendarContainer
+						type="classId"
+						id={student.class.id}
+					/> */}
 				</div>
 			</div>
 			{/* RIGHT */}
@@ -167,28 +186,28 @@ const SingleStudentPage = () => {
 					<h1 className="text-xl font-semibold">Shortcuts</h1>
 					<div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
 						<Link
-							className="p-3 rounded-md bg-faisal_dev_YellowLight"
-							href="/">
+							className="p-3 rounded-md bg-lamaSkyLight"
+							href={`/list/lessons?classId=${student.class.id}`}>
 							Student&apos;s Lessons
 						</Link>
 						<Link
-							className="p-3 rounded-md bg-faisal_dev_PurpleLight"
-							href="/">
+							className="p-3 rounded-md bg-lamaPurpleLight"
+							href={`/list/teachers?classId=${student.class.id}`}>
 							Student&apos;s Teachers
 						</Link>
 						<Link
 							className="p-3 rounded-md bg-pink-50"
-							href="/">
+							href={`/list/exams?classId=${student.class.id}`}>
 							Student&apos;s Exams
 						</Link>
 						<Link
-							className="p-3 rounded-md bg-faisal_dev_SeaGreenLight"
-							href="/">
+							className="p-3 rounded-md bg-lamaSkyLight"
+							href={`/list/assignments?classId=${student.class.id}`}>
 							Student&apos;s Assignments
 						</Link>
 						<Link
-							className="p-3 rounded-md bg-faisal_dev_YellowLight"
-							href="/">
+							className="p-3 rounded-md bg-lamaYellowLight"
+							href={`/list/results?studentId=${student.id}`}>
 							Student&apos;s Results
 						</Link>
 					</div>
--- src/components/TableSearch.tsx
+++ src/components/TableSearch.tsx
@@ -1,10 +1,27 @@
 /** @format */
 
+"use client";
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const TableSearch = () => {
+	const router = useRouter();
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		const value = (e.currentTarget[0] as HTMLInputElement).value;
+
+		const params = new URLSearchParams(window.location.search);
+		params.set("search", value);
+		router.push(`${window.location.pathname}?${params}`);
+	};
+
 	return (
-		<div className="w-full md:w-auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
+		<form
+			onSubmit={handleSubmit}
+			className="w-full md:w-auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
 			<Image
 				src="/search.png"
 				alt=""
@@ -16,7 +33,7 @@ const TableSearch = () => {
 				placeholder="Search..."
 				className="w-[200px] p-2 bg-transparent outline-none"
 			/>
-		</div>
+		</form>
 	);
 };
 
